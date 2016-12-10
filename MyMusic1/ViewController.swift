a542584928b46fd9cb44b74ede3589e74fa2575b
nao <mezase.nember.1@gmail.com>
2016-12-11 06:39:49 +0900

    Initial Commit

---
+++ MyMusic1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMusic1
+//
+//  Created by nao on 2016/12/11.
+//  Copyright © 2016年 com.naoya.MyMusic1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
